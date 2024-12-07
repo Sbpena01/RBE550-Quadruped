e324fb59ff45436870628f9539fea545b17a59f5
Sbpena01 <sbpena@wpi.edu>
2024-12-07 15:53:54 -0500

    Update .gitignore to ignore build, install, and log directories

--- build/leg_controls/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/jarothenberg/Documents/GitHub/RBE550-Quadruped/install/custom_interface/share/custom_interface/package.sh"
--- build/robot_sim/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jarothenberg/Documents/GitHub/RBE550-Quadruped/install/robot_sim'
